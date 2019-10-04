d66e17953b9ca173b36fb6570fbd337003154e40
Andrew Evans <asevans48@gmail.com>
2019-10-04 14:55:33 -0600

    working on json creator

---
+++ sql/json/ddlcreator.py
@@ -0,0 +1,15 @@
+"""
+Create a database layout from json
+
+@author Andrew Evans
+"""
+
+
+def create_layout(self, fpath, conn):
+    """
+    Create the database layout from provided DDL
+
+    :param fpath:   The fpath
+    :param conn:    The connection to use
+    """
+    pass
